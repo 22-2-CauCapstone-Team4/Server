a4378c5fc8aa62ce5cc553462e6dc54b77936038
kimgostring <kimgostring@naver.com>
2022-11-10 04:40:53 +0900

    [Add] 이메일, 닉네임 중복 확인 기능 추가

---
+++ functions/user/checkNicknameExisted.js
@@ -0,0 +1,14 @@
+exports = async function (nickname) {
+  const userInfos = context.services
+    .get('mongodb-atlas')
+    .db('AppData')
+    .collection('UserInfo');
+
+  try {
+    const user = await userInfos.findOne({nickname});
+    if (!user) return {success: true, isExisted: false};
+    else return {success: true, isExisted: true};
+  } catch (err) {
+    return {success: false, err: err.message};
+  }
+};
