2992960ba4544bc620ebff739a379f4119df7a2e
kimgostring <kimgostring@naver.com>
2022-12-17 00:11:00 +0900

    밀린 커밋 (serverless에 직접 작업했던 부분)

--- functions/friend/readFriends.js
+++ functions/friend/readFriends.js
@@ -3,12 +3,14 @@ exports = async function ({owner_id}) {
 
   const curStateColl = db.collection('CurState');
   const friendInfoColl = db.collection('Friend');
+  const missionColl = db.collection('Mission');
+  const goalColl = db.collection('Goal');
 
   const friendInfo = await friendInfoColl.findOne({owner_id});
   let friendIds = friendInfo.items;
   if (!friendIds) friendIds = [];
   else friendIds = friendIds.map(friend => friend.owner_id);
   const friendCurStates = await curStateColl.find({owner_id: {"$in": friendIds}});
-
+  
   return {friendInfo, friendCurStates};
 };
